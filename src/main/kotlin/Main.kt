958703e11132136e33290019aeb8ad8b6a2646ed
juanrodriguez2708 <you@example.com>
2025-09-10 23:26:59 -0500

    trabajo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,16 @@
 fun main() {
     val numbers = listOf(1, 2, 4, 7, 13, 9, 23)
 
-    // map applies the isPrime function to each element of the list
     val result = numbers.map { isPrime(it) }
 
     println("Numbers: $numbers")
     println("Are they prime?: $result")
 }
 
-/*
-TODO: Function that checks if a number is prime
- */
 fun isPrime(n: Int): Boolean {
-    return true;
-}
-
-
-/*
-TODO: Function that checks if a number is even
- */
-fun isEven(n: Int): Boolean {
-    return true;
+    if (n <= 1) return false
+    for (i in 2..Math.sqrt(n.toDouble()).toInt()) {
+        if (n % i == 0) return false
+    }
+    return true
 }
